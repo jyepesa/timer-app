790d5518283839ba8251377a2787073313d56ce7
jyepesa <juank_713@hotmail.com>
2025-12-03 23:50:07 +0100

    add files for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+const repoName = "timer-app";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: `/${repoName}/`,
   plugins: [react()],
-})
+});
